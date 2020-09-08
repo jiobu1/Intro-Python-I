59f1f561be3a7ebff3dd27b98bd7c5f436598250
Jisha Obukwelu <jisha@obukwelu.com>
2020-09-08 14:55:22 -0400

    Assignment completion

--- src/14_cal.py
+++ src/14_cal.py
@@ -31,13 +31,35 @@ import sys
 import calendar
 from datetime import datetime
 
-today = datetime.today()
-#https://www.programiz.com/python-programming/datetime/current-datetime
+# today = datetime.today()
+# #https://www.programiz.com/python-programming/datetime/current-datetime
 
-def print_calendar():
-  yy = int(input("Enter a year: "))
-  mm = int(input("Enter a month: "))
-  print(calendar.month(yy,mm))
+# def print_calendar():
+#   yy = int(input("Enter a year: "))
+#   mm = int(input("Enter a month: "))
+#   print(calendar.month(yy,mm))
 
-print_calendar()
+# print_calendar()
 
+l = len(sys.argv)
+
+if l == 1:
+  # User didn't specify any input
+  month = datetime.now().month
+  year = datetime.now().year
+elif l == 2:
+  # User didn't specify year
+  month = int(sys.argv[1])
+  year = datetime.now().year 
+elif l == 3:
+  # User specified both month and year
+  month = int(sys.argv[1])
+  year = int(sys.argv[2])
+else:
+  # User provided faulty input
+  print("usage: calendar.py [month] [year]")
+  sys.exit(1)
+
+cal = calendar.TextCalendar()
+
+cal.prmonth(year, month)
